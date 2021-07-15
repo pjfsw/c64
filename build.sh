335d046fefae7cb5e5f312e01510965e8b178ff7
Johan Fransson <per.johan.fransson@gmail.com>
2021-07-15 21:32:42 +0200

    Some coding

--- build.sh
+++ build.sh
@@ -2,5 +2,5 @@
 
 PRG=`echo $1 | sed s/\.asm/.prg/`
 PRG=`echo $PRG | sed s/src/target/`
-java -jar /Users/johanfr/priv/KickAssembler/KickAss.jar -odir ../target $1 && open -a /Applications/vice-sdl2-3.4-r37694/x64sc.app $PRG
+java -jar /Users/johanfr/priv/KickAssembler/KickAss.jar -odir ../target $1 && open -a /Applications/vice-gtk3-3.5/x64sc.app $PRG
 
